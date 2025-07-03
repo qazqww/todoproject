a5fff2e90e636f17139d52d1126d1167f25337ad
raptarior <qazqww@naver.com>
2025-07-03 19:04:46 +0900

    Add: TodoDetail

--- todofront/src/components/todo/TodoDetail.jsx
+++ todofront/src/components/todo/TodoDetail.jsx
@@ -1,9 +1,38 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+import dayjs from 'dayjs';
 
 const TodoDetail = ({ todo }) => {
+  const formatDate = (time) => {
+    return time ? dayjs(time).format('YY-MM-DD') : '-';
+  };
+  const formatTime = (time) => {
+    return time ? dayjs(time).format('HH:mm') : '';
+  };
+
   return (
-    <div>
-      <span className='detail-content'>{todo.detail}</span>
+    <div className='bg-amber-100 py-3'>
+      <div className='flex justify-between mx-10'>
+        <div className='detail-content'>{todo.detail}</div>
+        <table className='border-separate border-spacing-x-4'>
+          <tbody className='text-sm'>
+            <tr>
+              <td>기한</td>
+              <td>{formatDate(todo.ddayTime)}</td>
+              <td>{formatTime(todo.ddayTime)}</td>
+            </tr>
+            <tr>
+              <td>생성 시간</td>
+              <td>{formatDate(todo.createdTime)}</td>
+              <td>{formatTime(todo.createdTime)}</td>
+            </tr>
+            <tr>
+              <td>완료 시간</td>
+              <td>{formatDate(todo.doneTime)}</td>
+              <td>{formatTime(todo.doneTime)}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
