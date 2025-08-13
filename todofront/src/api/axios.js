05f3fd0015e33876d5c5093ad522c6380a08d4fa
raptarior <qazqww@naver.com>
2025-08-13 19:29:21 +0900

    MODIFY: 도메인 주소 수정

--- todofront/src/api/axios.js
+++ todofront/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://raptariortodoproj:8080/',
+  baseURL: 'http://raptariortodoproj.kro.kr:8080/',
   headers: {
     'Content-Type': 'application/json',
   },
