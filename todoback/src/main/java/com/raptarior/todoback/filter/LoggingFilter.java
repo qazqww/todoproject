28f7bc3c7fab24a63bb1b8282445f299850e3755
raptarior <qazqww@naver.com>
2025-08-14 17:05:22 +0900

    MODIFY: 로그 필터 logger로 수정 및 로깅 레벨 조절

--- todoback/src/main/java/com/raptarior/todoback/filter/LoggingFilter.java
+++ todoback/src/main/java/com/raptarior/todoback/filter/LoggingFilter.java
@@ -1,24 +1,27 @@
 package com.raptarior.todoback.filter;
 
-import jakarta.servlet.*;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 @Component
-public class LoggingFilter implements Filter {
+public class LoggingFilter extends OncePerRequestFilter {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoggingFilter.class);
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
 
-        HttpServletRequest req = (HttpServletRequest) request;
-        HttpServletResponse res = (HttpServletResponse) response;
-
-        System.out.println("Incoming request: " + req.getMethod() + " " + req.getRequestURI());
-        chain.doFilter(request, response);
-        System.out.println("Outgoing response: " + res.getStatus());
+        logger.info("Incoming request: {} {}", request.getMethod(), request.getRequestURI());
+        filterChain.doFilter(request, response);
+        logger.info("Response status: {}", response.getStatus());
     }
 }
