193a70b6e185f4086bacaf589a81cfdb1e710803
raptarior <qazqww@naver.com>
2025-08-18 17:49:08 +0900

    MODIFY: nginx 수정

--- todofront/src/api/axios.js
+++ todofront/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: '',
+  baseURL: 'https://raptariortodoproj.kro.kr',
+  withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
   },
